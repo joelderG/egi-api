a66b4f99da4657660c1130bbc12f9a7b28b47dc9
Joel <99019728+joelderG@users.noreply.github.com>
2026-03-16 16:34:01 +0100

    gitignore changes

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import xarray as xr
-import json
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
